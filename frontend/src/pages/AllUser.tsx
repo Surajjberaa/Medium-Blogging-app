5dd51c9c2e5c047407f2c74c9900df5f1b9dc4af
Suraj <berasuraj08@gmail.com>
2024-05-04 19:01:36 +0530

    Fixes #36

--- frontend/src/pages/AllUser.tsx
+++ frontend/src/pages/AllUser.tsx
@@ -1,4 +1,5 @@
 import AppBar from "@/components/AppBar"
+import BlogSkeleton from "@/components/BlogSkeleton";
 import UserCard from "@/components/UserCard"
 import { useAllUsers } from "@/hooks";
 
@@ -11,13 +12,39 @@ function AllUser() {
 
     if(loading){
         return <div>
-            loading....
+        <AppBar />
+        <div className="border-t-2  border-gray-400  flex justify-evenly">
+          <div className=" border-gray-600 w-full text-center hover:bg-gray-400 bg-slate-200" >
+            All Users
+          </div>
+          {/* <div className="border-l-2 border-gray-600 w-full text-center hover:bg-gray-400 bg-slate-200">
+            Likes
+          </div> */}
         </div>
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+         <BlogSkeleton />
+     </div>
     }
 
   return (
     <div>
         <AppBar/>
+        <div className="border-t-2  border-gray-400  flex justify-evenly">
+          <div className=" border-gray-600 w-full text-center hover:bg-gray-400 bg-slate-200" >
+            All Users
+          </div>
+          {/* <div className="border-l-2 border-gray-600 w-full text-center hover:bg-gray-400 bg-slate-200">
+            Likes
+          </div> */}
+        </div>
         <div className="flex justify-center   ">
             <div className=" lg:max-w-4xl w-[600px] max-w-xl px-7 lg:p-0 ">
                 {User.map(user => 
