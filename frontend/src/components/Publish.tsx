c68d7ca5d57186def9ab89bdb1473d9c2fd2a983
Suraj <berasuraj08@gmail.com>
2024-05-07 03:40:56 +0530

    Width responsiveness update in publish component alert

--- frontend/src/components/Publish.tsx
+++ frontend/src/components/Publish.tsx
@@ -51,7 +51,7 @@ function Publish() {
                 <div>
                     <button type="button" onClick={onclickHandler} className="text-white  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Publish Post</button>
                 </div>
-                <div className="mt-4 w-4/12">
+                <div className="mt-4 w-10/12 lg:w-4/12">
                     {alert == true ? <Alert desc="Please share your story first" onclick={removeAlert} />: null}
                 </div>
 
